b353049848a6dffd618e8f47cb623abedcf4cc1f
Austin Leydecker <arleydecker@renkin42.net>
2023-11-10 07:09:39 -0800

    Getting 500 error from second POST

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import os
 from datetime import datetime
+from datetime import timedelta
 
 def convert24(time_string):
     ampm = time_string[-1:]
@@ -37,10 +38,10 @@ res = s.post(login_url, data=payload)
 res_html = BeautifulSoup(res.text, "html.parser")
 days = res_html.find("div", {"id":"calendar"}).find("div", {"class":"dates"}).find("ul", {"class":"days"}).find_all("li", recursive=False)
 shift = 0
+now = datetime.now()
 for day in days:
     date = day.find("div", {"class":"date"})
     if date:
-        now = datetime.now()
         event_year = now.year
         event_month, event_day = date.get_text().split("/")
         event_month = int(event_month)
@@ -58,3 +59,34 @@ for day in days:
             print(event_start)
             event_end = datetime(event_year, event_month, event_day, end_hour, end_min)
             print(event_end)
+
+viewstate = res_html.find("input", {"name":"__VIEWSTATE"}).attrs["value"]
+viewstategen = res_html.find("input", {"name":"__VIEWSTATEGENERATOR"}).attrs["value"]
+offset = 7 - (now.weekday() + 1) % 7
+now += timedelta(days = offset)
+print(now.strftime("%m/%d/%Y"))
+payload = {
+    "ctl00$Master_ScriptManager":"ctl00$masterPlaceHolder$UpdatePanel1|ctl00$masterPlaceHolder$txtWeekPeriodDate",
+    "phTree":"ctl00_tpTransfer_phTree",
+    "Othervalue":"[Other...]",
+    "tempItemPlaceHolder":"ctl00_tpTransfer_tempPlaceHolder",
+    "tempSelectedChangeId":"ctl00_tpTransfer_tempSeletedChangeId",
+    "overlayPanel":"ctl00_tpTransfer_divTree",
+    "ctl00_tabContainer_ClientState":'{"ActiveTabIndex":0, "TabState":[true,true,true]}',
+    "ctl00$hdnActiveTab":"",
+    "ctl00$masterPlaceHolder$ddlDatePeriod":"SPECIFIC_DATE",
+    "ctl00$masterPlaceHolder$txtWeekPeriodDate":now.strftime("%m/%d/%Y"),
+    "__EVENTTARGET":"ctl00$masterPlaceHolder$txtWeekPeriodDate",
+    "__EVENTARGUMENT":"",
+    "__LASTFOCUS":"",
+    "__VIEWSTATE":viewstate,
+    "__VIEWSTATEGENERATOR":viewstategen,
+    "ActiveTab":"ctl00_hdnActiveTab",
+    "__AjaxControlToolkitCalendarCssLoaded":"",
+    "__ASYNCPOST":"true"
+}
+res = s.post(res.url, data=payload)
+res_html = BeautifulSoup(res.text, "html.parser")
+
+print(res.text)
+
